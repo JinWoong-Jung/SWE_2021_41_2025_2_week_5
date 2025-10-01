fddc44594dac8f7cb8ff864c932e2fbf217be11d
JUNG JIN WOONG <jinwoong1010@gmail.com>
2025-10-01 18:36:54 +0900

    edit error

--- arrange.sh
+++ arrange.sh
@@ -2,9 +2,9 @@
 
 for file in files/*; do
 
-    filename$(basename "$file")
+    filename=$(basename "$file")
 
-    first_char = $(echo "${filename:0:1}" | tr 'A-Z' 'a-z')
+    first_char=$(echo "${filename:0:1}" | tr 'A-Z' 'a-z')
 
     if [[ -d "$first_char" ]]; then
         mv "$file" "$first_char/"
