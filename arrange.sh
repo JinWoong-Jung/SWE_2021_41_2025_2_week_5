ae3840cbc674a0c58815eb2119d4bdb40956d461
JUNG JIN WOONG <jinwoong1010@gmail.com>
2025-10-01 18:29:04 +0900

    upload arrange.sh file

---
+++ arrange.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for file in files/*; do
+
+    filename$(basename "$file")
+
+    first_char = $(echo "${filename:0:1}" | tr 'A-Z' 'a-z')
+
+    if [[ -d "$first_char" ]]; then
+        mv "$file" "$first_char/"
+    fi
+
+done
